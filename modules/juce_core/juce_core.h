deb234ee3fca0093feb24448d26b603886a86c2f
kunitoki <kunitoki@gmail.com>
2024-04-26 09:32:59 +0200

    Switch to msvc building

--- modules/juce_core/juce_core.h
+++ modules/juce_core/juce_core.h
@@ -60,7 +60,7 @@
   OSXFrameworks:      Cocoa Foundation IOKit Security
   iOSFrameworks:      Foundation
   linuxLibs:          rt dl pthread
-  mingwLibs:          uuid kernel32 user32 wsock32 advapi32 wininet version ole32 ws2_32 oleaut32 imm32 comdlg32 shlwapi rpcrt4 winmm
+  mingwLibs:          ws2_32 uuid wininet version kernel32 user32 wsock32 advapi32 ole32 oleaut32 imm32 comdlg32 shlwapi rpcrt4 winmm
 
  END_JUCE_MODULE_DECLARATION
 
