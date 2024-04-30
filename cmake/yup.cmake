83038430c606028c07cd5eefc21df6e972f822aa
kunitoki <kunitoki@gmail.com>
2024-04-30 10:56:14 +0200

    Remove find glfw3

--- cmake/yup.cmake
+++ cmake/yup.cmake
@@ -19,7 +19,7 @@
 
 #==============================================================================
 
-list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/tools")
+#list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/tools")
 
 #==============================================================================
 
