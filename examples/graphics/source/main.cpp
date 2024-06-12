776d53360d67d040dfb032b9d141f609d2e4b112
kunitoki <kunitoki@gmail.com>
2024-06-12 21:16:14 +0200

    Fix graphics example

--- examples/graphics/source/main.cpp
+++ examples/graphics/source/main.cpp
@@ -29,222 +29,6 @@
 
 //==============================================================================
 
-class TextButton : public yup::Button
-{
-public:
-    TextButton (yup::StringRef componentID, const yup::Font& font)
-         : Button (componentID)
-         , font (font)
-    {
-    }
-
-    void paintButton (yup::Graphics& g, bool isButtonOver, bool isButtonDown) override
-    {
-        auto bounds = getLocalBounds().reduced (proportionOfWidth (0.01f));
-        const auto center = bounds.getCenter();
-
-        yup::Path backgroundPath;
-        //backgroundPath.clear();
-        backgroundPath.addRoundedRectangle (bounds.reduced (proportionOfWidth (0.045f)), 10.0f, 10.0f, 10.0f, 10.0f);
-        g.setFillColor (isButtonDown ? yup::Color (0xff000000) : yup::Color (0xffffffff));
-        g.fillPath (backgroundPath);
-
-        yup::StyledText text;
-        //text.clear();
-        text.appendText (font, bounds.getHeight() * 0.5f, bounds.getHeight() * 0.5f, getComponentID().toRawUTF8());
-        text.layout (bounds.reduced (0.0f, 10.0f), yup::StyledText::center);
-
-        g.setStrokeColor (isButtonDown ? yup::Color (0xffffffff) : yup::Color (0xff000000));
-        g.strokeFittedText (text, {});
-    }
-
-private:
-    const yup::Font& font;
-};
-
-//==============================================================================
-
-class CustomSlider : public yup::Component
-{
-public:
-    CustomSlider (int index, const yup::Font& font)
-         : index (index)
-         , font (font)
-    {
-        setTitle (yup::String (index));
-        setValue (0.0f);
-    }
-
-    void resized() override
-    {
-        updateRenderItems (true);
-    }
-
-    void mouseEnter (const yup::MouseEvent& event) override
-    {
-        isInside = true;
-
-        repaint();
-    }
-
-    void mouseExit (const yup::MouseEvent& event) override
-    {
-        isInside = false;
-
-        repaint();
-    }
-
-    void mouseDown (const yup::MouseEvent& event) override
-    {
-        origin = event.getPosition();
-
-        takeFocus();
-
-        repaint();
-    }
-
-    void mouseUp (const yup::MouseEvent& event) override
-    {
-    }
-
-    void mouseDrag (const yup::MouseEvent& event) override
-    {
-        //auto [x, y] = event.getPosition();
-
-        const float multiplier =
-            (event.getModifiers().isShiftDown() || event.isRightButtonDown()) ? 0.0001f : 0.0025f;
-
-        const float distance = -origin.verticalDistanceTo (event.getPosition()) * multiplier;
-
-        origin = event.getPosition();
-
-        setValue (value + distance);
-
-        repaint();
-    }
-
-    void mouseWheel (const yup::MouseEvent& event, const yup::MouseWheelData& data) override
-    {
-        //auto [x, y] = event.getPosition();
-
-        const float multiplier = event.getModifiers().isShiftDown() ? 0.001f : 0.0025f;
-        const float distance = (data.getDeltaX() + data.getDeltaY()) * multiplier;
-
-        origin = event.getPosition();
-
-        setValue (value + distance);
-
-        repaint();
-    }
-
-    void paint (yup::Graphics& g) override
-    {
-        auto bounds = getLocalBounds().reduced (proportionOfWidth (0.1f));
-
-        g.setFillColor (yup::Color (0xff3d3d3d));
-        g.fillPath (backgroundPath);
-
-        g.setStrokeColor (yup::Color (0xff2b2b2b));
-        g.setStrokeWidth (proportionOfWidth (0.0175f));
-        g.strokePath (backgroundPath);
-
-        g.setStrokeCap (yup::StrokeCap::Round);
-        g.setStrokeColor (yup::Color (0xff636363));
-        g.setStrokeWidth (proportionOfWidth (0.075f));
-        g.strokePath (backgroundArc);
-
-        g.setStrokeCap (yup::StrokeCap::Round);
-        g.setStrokeColor (isInside ? yup::Color (0xff4ebfff).brighter (0.3f) : yup::Color (0xff4ebfff));
-        g.setStrokeWidth (proportionOfWidth (0.075f));
-        g.strokePath (foregroundArc);
-
-        g.setStrokeCap (yup::StrokeCap::Round);
-        g.setStrokeColor (yup::Color (0xffffffff));
-        g.setStrokeWidth (proportionOfWidth (0.03f));
-        g.strokePath (foregroundLine);
-
-        g.setStrokeColor (yup::Color (0xffffffff));
-        g.strokeFittedText (text, getLocalBounds().reduced (5).removeFromBottom (proportionOfWidth (0.1f)));
-
-        //if (hasFocus())
-        //{
-        //    g.setStrokeColor (yup::Color (0xffff5f2b));
-        //    g.strokeRect (getLocalBounds(), proportionOfWidth (0.0175f));
-        //}
-    }
-
-    void setValue (float newValue)
-    {
-        value = yup::jlimit (0.0f, 1.0f, newValue);
-
-        valueChanged();
-
-        updateRenderItems (false);
-    }
-
-    virtual void valueChanged()
-    {
-    }
-
-private:
-    void updateRenderItems (bool forceAll)
-    {
-        auto bounds = getLocalBounds().reduced (proportionOfWidth (0.1f));
-        const auto center = bounds.getCenter();
-
-        constexpr auto fromRadians = yup::degreesToRadians(135.0f);
-        constexpr auto toRadians = fromRadians + yup::degreesToRadians(270.0f);
-
-        if (forceAll)
-        {
-            backgroundPath.clear();
-            backgroundPath.addEllipse (bounds.reduced (proportionOfWidth (0.105f)));
-
-            backgroundArc.clear();
-            backgroundArc.addCenteredArc (center,
-                                          bounds.getWidth() / 2.0f, bounds.getHeight() / 2.0f, 0.0f,
-                                          fromRadians, toRadians, true);
-        }
-
-        const auto toCurrentRadians = fromRadians + yup::degreesToRadians(270.0f) * value;
-
-        foregroundArc.clear();
-        foregroundArc.addCenteredArc (center,
-                                      bounds.getWidth() / 2.0f, bounds.getHeight() / 2.0f, 0.0f,
-                                      fromRadians, toCurrentRadians, true);
-
-        const auto reducedBounds = bounds.reduced (proportionOfWidth (0.175f));
-        const auto pos = center.getPointOnCircumference (
-            reducedBounds.getWidth() / 2.0f,
-            reducedBounds.getHeight() / 2.0f,
-            toCurrentRadians);
-
-        foregroundLine.clear();
-        foregroundLine.addLine (yup::Line<float> (pos, center).keepOnlyStart (0.25f));
-
-        text.clear();
-        text.appendText (font, proportionOfHeight(0.1f), proportionOfHeight(0.1f), yup::String (value, 3).toRawUTF8());
-        text.layout (getLocalBounds().reduced (5).removeFromBottom (proportionOfWidth (0.1f)), yup::StyledText::center);
-    }
-
-    struct
-    {
-        yup::Path backgroundPath;
-        yup::Path backgroundArc;
-        yup::Path foregroundArc;
-        yup::Path foregroundLine;
-        yup::StyledText text;
-    };
-
-    yup::Point<float> origin;
-    const yup::Font& font;
-    float value = 0.0f;
-    int index = 0;
-    bool isInside = false;
-};
-
-//==============================================================================
-
 class CustomWindow
     : public yup::DocumentWindow
     , public yup::Timer
@@ -279,11 +63,11 @@ public:
         for (int i = 0; i < totalRows * totalColumns; ++i)
             addAndMakeVisible (sliders.add (std::make_unique<yup::Slider> (yup::String (i), font)));
 
-        //button = std::make_unique<yup::TextButton> ("xyz", font);
-        //addAndMakeVisible (*button);
+        button = std::make_unique<yup::TextButton> ("xyz", font);
+        addAndMakeVisible (*button);
 
-        //deviceManager.addAudioCallback (this);
-        //deviceManager.initialiseWithDefaultDevices (1, 0);
+        deviceManager.addAudioCallback (this);
+        deviceManager.initialiseWithDefaultDevices (1, 0);
 
         startTimerHz (1);
     }
@@ -293,7 +77,7 @@ public:
         inputReady.signal();
         renderReady.signal();
 
-        //deviceManager.closeAudioDevice();
+        deviceManager.closeAudioDevice();
     }
 
     void resized() override
@@ -319,29 +103,6 @@ public:
     void paint (yup::Graphics& g) override
     {
         yup::DocumentWindow::paint (g);
-
-        /*
-        yup::String svgPathData =
-            "M 250,0 "
-            "L 309,181 "
-            "L 500,181 "
-            "L 351,293 "
-            "L 405,475 "
-            "L 250,375 "
-            "L 95,475 "
-            "L 149,293 "
-            "L 0,181 "
-            "L 191,181 "
-            "Z";
-
-        yup::Path yupPath;
-
-        if (yupPath.parsePathData (svgPathData))
-        {
-            g.setColor (0xff4b4bff);
-            g.drawPath (yupPath, 5.0f);
-        }
-        */
 
         /*
         //inputReady.wait();
