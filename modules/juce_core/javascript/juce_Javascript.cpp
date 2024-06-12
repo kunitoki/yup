0789377b7efa38aa05e6f70d7b881dc28b5215a9
kunitoki <kunitoki@gmail.com>
2024-06-12 12:00:56 +0200

    Added tests for JavascriptEngine

--- modules/juce_core/javascript/juce_Javascript.cpp
+++ modules/juce_core/javascript/juce_Javascript.cpp
@@ -59,7 +59,7 @@ namespace juce
     X(var,      "var")      X(if_,     "if")     X(else_,  "else")   X(do_,       "do")       X(null_,     "null") \
     X(while_,   "while")    X(for_,    "for")    X(break_, "break")  X(continue_, "continue") X(undefined, "undefined") \
     X(function, "function") X(return_, "return") X(true_,  "true")   X(false_,    "false")    X(new_,      "new") \
-    X(typeof_,  "typeof")
+    X(typeof_,  "typeof")   X(throw_,  "throw")
 
 namespace TokenTypes
 {
@@ -367,6 +367,19 @@ struct JavascriptEngine::RootObject final : public DynamicObject
         }
 
         ExpPtr returnValue;
+    };
+
+    struct ThrowStatement  : public Statement
+    {
+        ThrowStatement (const CodeLocation& l, Expression* v) noexcept : Statement (l), thrownValue (v) {}
+
+        ResultCode perform (const Scope& s, var*) const override
+        {
+            location.throwError(thrownValue->getResult (s));
+            return ok;
+        }
+
+        ExpPtr thrownValue;
     };
 
     struct BreakStatement final : public Statement
@@ -1150,6 +1163,7 @@ struct JavascriptEngine::RootObject final : public DynamicObject
             if (matchIf (TokenTypes::do_))              return parseDoOrWhileLoop (true);
             if (matchIf (TokenTypes::for_))             return parseForLoop();
             if (matchIf (TokenTypes::return_))          return parseReturn();
+            if (matchIf (TokenTypes::throw_))           return parseThrow();
             if (matchIf (TokenTypes::break_))           return new BreakStatement (location);
             if (matchIf (TokenTypes::continue_))        return new ContinueStatement (location);
             if (matchIf (TokenTypes::function))         return parseFunction();
@@ -1187,6 +1201,16 @@ struct JavascriptEngine::RootObject final : public DynamicObject
                 return new ReturnStatement (location, new Expression (location));
 
             auto* r = new ReturnStatement (location, parseExpression());
+            matchIf (TokenTypes::semicolon);
+            return r;
+        }
+
+        Statement* parseThrow()
+        {
+            if (matchIf (TokenTypes::semicolon))
+                return new ThrowStatement (location, new Expression (location));
+
+            auto* r = new ThrowStatement (location, parseExpression());
             matchIf (TokenTypes::semicolon);
             return r;
         }
@@ -1897,7 +1921,10 @@ var JavascriptEngine::callFunction (const Identifier& function, const var::Nativ
     {
         prepareTimeout();
         if (result != nullptr) *result = Result::ok();
-        RootObject::Scope ({}, *root, *root).findAndInvokeMethod (function, args, returnVal);
+
+        auto invokeResult = RootObject::Scope ({}, *root, *root).findAndInvokeMethod (function, args, returnVal);
+        if (! invokeResult && result != nullptr)
+            *result = Result::fail ("Unable to find method '" + function.toString() + "'");
     }
     catch (String& error)
     {
