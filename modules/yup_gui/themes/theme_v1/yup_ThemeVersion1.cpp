27f945051545313913e4c94b94ab0c34004e9656
kunitoki <kunitoki@gmail.com>
2024-08-01 15:55:55 +0200

    Fix formatting

--- modules/yup_gui/themes/theme_v1/yup_ThemeVersion1.cpp
+++ modules/yup_gui/themes/theme_v1/yup_ThemeVersion1.cpp
@@ -39,82 +39,87 @@ ApplicationTheme::Ptr createThemeVersion1()
 
     theme->setDefaultFont (std::move (font));
 
-    theme->setComponentTheme<Slider::Theme> ({ [theme] (Graphics& g, const Slider& s)
-                                               {
-                                                   auto bounds = s.getLocalBounds().reduced (s.proportionOfWidth (0.1f));
-                                                   const auto center = bounds.getCenter();
+    // clang-format off
+    theme->setComponentTheme<Slider::Theme> (
+    {
+        [theme] (Graphics& g, const Slider& s)
+        {
+            auto bounds = s.getLocalBounds().reduced (s.proportionOfWidth (0.1f));
+            const auto center = bounds.getCenter();
 
-                                                   constexpr auto fromRadians = degreesToRadians (135.0f);
-                                                   constexpr auto toRadians = fromRadians + degreesToRadians (270.0f);
+            constexpr auto fromRadians = degreesToRadians (135.0f);
+            constexpr auto toRadians = fromRadians + degreesToRadians (270.0f);
 
-                                                   Path backgroundPath;
-                                                   backgroundPath.addEllipse (bounds.reduced (s.proportionOfWidth (0.105f)));
+            Path backgroundPath;
+            backgroundPath.addEllipse (bounds.reduced (s.proportionOfWidth (0.105f)));
 
-                                                   g.setFillColor (Color (0xff3d3d3d));
-                                                   g.fillPath (backgroundPath);
+            g.setFillColor (Color (0xff3d3d3d));
+            g.fillPath (backgroundPath);
 
-                                                   g.setStrokeColor (Color (0xff2b2b2b));
-                                                   g.setStrokeWidth (s.proportionOfWidth (0.0175f));
-                                                   g.strokePath (backgroundPath);
+            g.setStrokeColor (Color (0xff2b2b2b));
+            g.setStrokeWidth (s.proportionOfWidth (0.0175f));
+            g.strokePath (backgroundPath);
 
-                                                   Path backgroundArc;
-                                                   backgroundArc.addCenteredArc (center,
-                                                                                 bounds.getWidth() / 2.0f,
-                                                                                 bounds.getHeight() / 2.0f,
-                                                                                 0.0f,
-                                                                                 fromRadians,
-                                                                                 toRadians,
-                                                                                 true);
+            Path backgroundArc;
+            backgroundArc.addCenteredArc (center,
+                                          bounds.getWidth() / 2.0f,
+                                          bounds.getHeight() / 2.0f,
+                                          0.0f,
+                                          fromRadians,
+                                          toRadians,
+                                          true);
 
-                                                   g.setStrokeCap (StrokeCap::Round);
-                                                   g.setStrokeColor (Color (0xff636363));
-                                                   g.setStrokeWidth (s.proportionOfWidth (0.075f));
-                                                   g.strokePath (backgroundArc);
+            g.setStrokeCap (StrokeCap::Round);
+            g.setStrokeColor (Color (0xff636363));
+            g.setStrokeWidth (s.proportionOfWidth (0.075f));
+            g.strokePath (backgroundArc);
 
-                                                   const auto toCurrentRadians = fromRadians + degreesToRadians (270.0f) * s.getValue();
+            const auto toCurrentRadians = fromRadians + degreesToRadians (270.0f) * s.getValue();
 
-                                                   Path foregroundArc;
-                                                   foregroundArc.addCenteredArc (center,
-                                                                                 bounds.getWidth() / 2.0f,
-                                                                                 bounds.getHeight() / 2.0f,
-                                                                                 0.0f,
-                                                                                 fromRadians,
-                                                                                 toCurrentRadians,
-                                                                                 true);
+            Path foregroundArc;
+            foregroundArc.addCenteredArc (center,
+                                          bounds.getWidth() / 2.0f,
+                                          bounds.getHeight() / 2.0f,
+                                          0.0f,
+                                          fromRadians,
+                                          toCurrentRadians,
+                                          true);
 
-                                                   g.setStrokeCap (StrokeCap::Round);
-                                                   g.setStrokeColor (s.isMouseOver() ? Color (0xff4ebfff).brighter (0.3f) : Color (0xff4ebfff));
-                                                   g.setStrokeWidth (s.proportionOfWidth (0.075f));
-                                                   g.strokePath (foregroundArc);
+            g.setStrokeCap (StrokeCap::Round);
+            g.setStrokeColor (s.isMouseOver() ? Color (0xff4ebfff).brighter (0.3f) : Color (0xff4ebfff));
+            g.setStrokeWidth (s.proportionOfWidth (0.075f));
+            g.strokePath (foregroundArc);
 
-                                                   const auto reducedBounds = bounds.reduced (s.proportionOfWidth (0.175f));
-                                                   const auto pos = center.getPointOnCircumference (
-                                                       reducedBounds.getWidth() / 2.0f,
-                                                       reducedBounds.getHeight() / 2.0f,
-                                                       toCurrentRadians);
+            const auto reducedBounds = bounds.reduced (s.proportionOfWidth (0.175f));
+            const auto pos = center.getPointOnCircumference (
+                reducedBounds.getWidth() / 2.0f,
+                reducedBounds.getHeight() / 2.0f,
+                toCurrentRadians);
 
-                                                   Path foregroundLine;
-                                                   foregroundLine.addLine (Line<float> (pos, center).keepOnlyStart (0.25f));
+            Path foregroundLine;
+            foregroundLine.addLine (Line<float> (pos, center).keepOnlyStart (0.25f));
 
-                                                   g.setStrokeCap (StrokeCap::Round);
-                                                   g.setStrokeColor (Color (0xffffffff));
-                                                   g.setStrokeWidth (s.proportionOfWidth (0.03f));
-                                                   g.strokePath (foregroundLine);
+            g.setStrokeCap (StrokeCap::Round);
+            g.setStrokeColor (Color (0xffffffff));
+            g.setStrokeWidth (s.proportionOfWidth (0.03f));
+            g.strokePath (foregroundLine);
 
-                                                   StyledText text;
-                                                   text.appendText (theme->getDefaultFont(), s.proportionOfHeight (0.1f), s.proportionOfHeight (0.1f), String (s.getValue(), 3).toRawUTF8());
-                                                   text.layout (s.getLocalBounds().reduced (5).removeFromBottom (s.proportionOfWidth (0.1f)), StyledText::center);
+            StyledText text;
+            text.appendText (theme->getDefaultFont(), s.proportionOfHeight (0.1f), s.proportionOfHeight (0.1f), String (s.getValue(), 3).toRawUTF8());
+            text.layout (s.getLocalBounds().reduced (5).removeFromBottom (s.proportionOfWidth (0.1f)), StyledText::center);
 
-                                                   g.setStrokeColor (Color (0xffffffff));
-                                                   g.strokeFittedText (text, s.getLocalBounds().reduced (5).removeFromBottom (s.proportionOfWidth (0.1f)));
+            g.setStrokeColor (Color (0xffffffff));
+            g.strokeFittedText (text, s.getLocalBounds().reduced (5).removeFromBottom (s.proportionOfWidth (0.1f)));
 
-                                                   if (s.hasFocus())
-                                                   {
-                                                       g.setStrokeColor (Color (0xffff5f2b));
-                                                       g.setStrokeWidth (s.proportionOfWidth (0.0175f));
-                                                       g.strokeRect (s.getLocalBounds());
-                                                   }
-                                               } });
+            if (s.hasFocus())
+            {
+                g.setStrokeColor (Color (0xffff5f2b));
+                g.setStrokeWidth (s.proportionOfWidth (0.0175f));
+                g.strokeRect (s.getLocalBounds());
+            }
+        }
+    });
+    // clang-format on
 
     return theme;
 }
