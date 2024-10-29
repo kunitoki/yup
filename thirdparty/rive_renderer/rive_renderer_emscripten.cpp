5674de9a3059e2d0599675da73b863c0658f8f53
kunitoki <kunitoki@gmail.com>
2024-10-29 23:38:07 +0100

    Fix emscripten

--- thirdparty/rive_renderer/rive_renderer_emscripten.cpp
+++ thirdparty/rive_renderer/rive_renderer_emscripten.cpp
@@ -23,19 +23,20 @@
 
 #if RIVE_WEBGPU
 #include "source/webgpu/em_js_handle.cpp"
-#include "source/webgpu/pls_render_context_webgpu_impl.cpp"
+#include "source/webgpu/render_context_webgpu_impl.cpp"
+//#include "source/webgpu/render_context_webgpu_vulkan.cpp"
 
 #elif RIVE_WEBGL
+#include "source/gl/gl_state.cpp"
+#include "source/gl/gl_utils.cpp"
 #include "source/gl/load_gles_extensions.cpp"
-//#include "source/gl/load_store_actions_ext.cpp"
+#include "source/gl/load_store_actions_ext.cpp"
 //#include "source/gl/pls_impl_ext_native.cpp"
 //#include "source/gl/pls_impl_framebuffer_fetch.cpp"
 //#include "source/gl/pls_impl_rw_texture.cpp"
 #include "source/gl/pls_impl_webgl.cpp"
-#include "source/gl/pls_render_buffer_gl_impl.cpp"
-#include "source/gl/pls_render_context_gl_impl.cpp"
-#include "source/gl/gl_utils.cpp"
-#include "source/gl/pls_render_target_gl.cpp"
-#include "source/gl/gl_state.cpp"
+#include "source/gl/render_buffer_gl_impl.cpp"
+#include "source/gl/render_context_gl_impl.cpp"
+#include "source/gl/render_target_gl.cpp"
 
 #endif
