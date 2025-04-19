e2d44cb10c61fcb703ebe624b79f07668f5638c5
kunitoki <kunitoki@gmail.com>
2025-04-19 23:16:58 +0200

    More doxygen

--- modules/yup_gui/mouse/yup_MouseCursor.h
+++ modules/yup_gui/mouse/yup_MouseCursor.h
@@ -23,9 +23,20 @@ namespace yup
 {
 
 //==============================================================================
+/** Represents a mouse cursor image.
+
+    This class is used to set the mouse cursor's appearance. You can create a custom
+    cursor from an image, or use one of the standard system cursors.
+
+    @see Component::setMouseCursor, Component::getMouseCursor
+*/
 class JUCE_API MouseCursor
 {
 public:
+    /** The set of available standard system mouse cursors.
+
+        @see MouseCursor::MouseCursor
+    */
     enum Type
     {
         None = 0,
@@ -45,20 +56,51 @@ public:
     };
 
     //==============================================================================
+    /** Creates a default mouse cursor (an arrow). */
     MouseCursor() noexcept = default;
+
+    /** Creates a standard system cursor.
+
+        @param type    The type of cursor to create - this can be one of the values
+                      from the Type enum.
+        @see Type
+    */
     MouseCursor (Type newType) noexcept;
 
     //==============================================================================
+    /** Copy constructor. */
     MouseCursor (const MouseCursor& other) noexcept = default;
+
+    /** Move constructor. */
     MouseCursor (MouseCursor&& other) noexcept = default;
+
+    /** Copy assignment operator. */
     MouseCursor& operator= (const MouseCursor& other) noexcept = default;
+
+    /** Move assignment operator. */
     MouseCursor& operator= (MouseCursor&& other) noexcept = default;
 
     //==============================================================================
+    /** Checks if two cursors are the same.
+
+        @param other    The other cursor to compare with
+        @returns       True if the cursors are identical, false otherwise
+    */
     bool operator== (const MouseCursor& other) const noexcept;
+
+    /** Checks if two cursors are different.
+
+        @param other    The other cursor to compare with
+        @returns       True if the cursors are different, false if they're identical
+    */
     bool operator!= (const MouseCursor& other) const noexcept;
 
     //==============================================================================
+    /** Returns the type of this cursor.
+
+        @returns    The cursor's type, as set when it was created.
+        @see Type
+    */
     Type getType() const noexcept { return currentType; }
 
 private:
--- modules/yup_gui/mouse/yup_MouseEvent.h
+++ modules/yup_gui/mouse/yup_MouseEvent.h
@@ -25,61 +25,220 @@ namespace yup
 class JUCE_API Component;
 
 //==============================================================================
+/** Contains information about a mouse event.
+
+    This class is used to represent mouse events such as clicks, moves, drags, etc.
+    It contains information about the mouse state, position, and which component
+    the event occurred on.
+
+    A MouseEvent object is passed to a component's mouse event methods, where you can
+    query its properties to find out what the user is doing.
+
+    @see Component::mouseMove, Component::mouseEnter, Component::mouseExit,
+         Component::mouseDown, Component::mouseUp, Component::mouseDrag
+*/
 class JUCE_API MouseEvent
 {
 public:
     //==============================================================================
+    /** Constants to indicate which mouse buttons are being held down.
+
+        @see getButtons, isLeftButtonDown, isRightButtonDown, isMiddleButtonDown
+    */
     enum Buttons
     {
-        noButtons = 0x0000,
-        leftButton = 0x0001,
-        middleButton = 0x0002,
-        rightButton = 0x0004,
+        noButtons = 0x0000,      /**< No buttons pressed. */
+        leftButton = 0x0001,     /**< The left mouse button. */
+        middleButton = 0x0002,   /**< The middle mouse button. */
+        rightButton = 0x0004,    /**< The right mouse button. */
 
-        allButtons = leftButton | middleButton | rightButton,
+        allButtons = leftButton | middleButton | rightButton  /**< Bitmask of all buttons. */
     };
 
     //==============================================================================
+    /** Creates a default MouseEvent object.
+
+        This creates an event describing a mouse movement to position (0, 0), with
+        no buttons pressed and no key modifiers.
+    */
     MouseEvent() noexcept = default;
+
+    /** Creates a MouseEvent object.
+
+        @param newButtons        The buttons that are currently held down
+        @param newModifiers     The key modifiers that are currently active
+        @param newPosition      The mouse position, relative to the component that receives the event
+    */
     MouseEvent (Buttons newButtons, KeyModifiers newModifiers, const Point<float>& newPosition) noexcept;
+
+    /** Creates a MouseEvent object.
+
+        @param newButtons        The buttons that are currently held down
+        @param newModifiers     The key modifiers that are currently active
+        @param newPosition      The mouse position, relative to the component that receives the event
+        @param sourceComponent  The component that the mouse event applies to
+    */
     MouseEvent (Buttons newButtons, KeyModifiers newModifiers, const Point<float>& newPosition, Component* sourceComponent) noexcept;
 
     //==============================================================================
+    /** Copy constructor. */
     MouseEvent (const MouseEvent& other) noexcept = default;
+
+    /** Move constructor. */
     MouseEvent (MouseEvent&& other) noexcept = default;
+
+    /** Copy assignment operator. */
     MouseEvent& operator= (const MouseEvent& other) noexcept = default;
+
+    /** Move assignment operator. */
     MouseEvent& operator= (MouseEvent&& other) noexcept = default;
 
     //==============================================================================
+    /** Returns true if the left mouse button is currently held down.
+
+        @returns    true if the left button is down
+    */
     bool isLeftButtoDown() const noexcept;
+
+    /** Returns true if the middle mouse button is currently held down.
+
+        @returns    true if the middle button is down
+    */
     bool isMiddleButtonDown() const noexcept;
+
+    /** Returns true if the right mouse button is currently held down.
+
+        @returns    true if the right button is down
+    */
     bool isRightButtonDown() const noexcept;
+
+    /** Returns true if any mouse button is currently held down.
+
+        @returns    true if any button is down
+    */
     bool isAnyButtonDown() const noexcept;
+
+    /** Returns the current mouse button state.
+
+        @returns    a bitmask of the buttons that are currently held down
+
+        @see Buttons
+    */
     Buttons getButtons() const noexcept;
+
+    /** Creates a copy of this event with the specified buttons added.
+
+        @param buttonsToAdd    the buttons to add to the new event
+
+        @returns              a new MouseEvent object
+    */
     MouseEvent withButtons (Buttons buttonsToAdd) const noexcept;
+
+    /** Creates a copy of this event with the specified buttons removed.
+
+        @param buttonsToRemove    the buttons to remove from the new event
+
+        @returns                 a new MouseEvent object
+    */
     MouseEvent withoutButtons (Buttons buttonsToRemove) const noexcept;
 
     //==============================================================================
+    /** Returns the key modifiers associated with this mouse event.
+
+        @returns    the key modifiers currently active
+    */
     KeyModifiers getModifiers() const noexcept;
+
+    /** Creates a copy of this event with different modifiers.
+
+        @param newModifiers    the new modifier flags to use
+
+        @returns              a new MouseEvent object
+    */
     MouseEvent withModifiers (KeyModifiers newModifiers) const noexcept;
 
     //==============================================================================
+    /** Returns the mouse position associated with this event.
+
+        @returns    the mouse position, relative to the component that received the event
+    */
     Point<float> getPosition() const noexcept;
+
+    /** Creates a copy of this event with a different position.
+
+        @param newPosition    the new position to use
+
+        @returns            a new MouseEvent object
+    */
     MouseEvent withPosition (const Point<float>& newPosition) const noexcept;
+
+    /** Creates a copy of this event with its position offset by the specified amount.
+
+        @param translation    the offset to apply to the position
+
+        @returns            a new MouseEvent object
+    */
     MouseEvent withTranslatedPosition (const Point<float>& translation) const noexcept;
 
     //==============================================================================
+    /** Returns the position at which the last mouse-down event occurred.
+
+        @returns    the last mouse-down position, relative to the component that received the event
+    */
     Point<float> getLastMouseDownPosition() const noexcept;
+
+    /** Creates a copy of this event with a different last mouse-down position.
+
+        @param newPosition    the new last mouse-down position to use
+
+        @returns            a new MouseEvent object
+    */
     MouseEvent withLastMouseDownPosition (const Point<float>& newPosition) const noexcept;
+
+    /** Returns the time at which the last mouse-down event occurred.
+
+        @returns    the time of the last mouse-down event
+    */
     juce::Time getLastMouseDownTime() const noexcept;
+
+    /** Creates a copy of this event with a different last mouse-down time.
+
+        @param newTime    the new time to use
+
+        @returns        a new MouseEvent object
+    */
     MouseEvent withLastMouseDownTime (juce::Time newTime) const noexcept;
 
     //==============================================================================
+    /** Returns the component that this event applies to.
+
+        @returns    the component that the event occurred on
+    */
     Component* getSourceComponent() const noexcept;
+
+    /** Creates a copy of this event with a different source component.
+
+        @param newComponent    the new component to use as the source
+
+        @returns             a new MouseEvent object
+    */
     MouseEvent withSourceComponent (Component* newComponent) const noexcept;
 
     //==============================================================================
+    /** Compares two MouseEvent objects.
+
+        @param other    the other event to compare with
+
+        @returns       true if the events are identical
+    */
     bool operator== (const MouseEvent& other) const noexcept;
+
+    /** Compares two MouseEvent objects.
+
+        @param other    the other event to compare with
+
+        @returns       true if the events are different
+    */
     bool operator!= (const MouseEvent& other) const noexcept;
 
 private:
