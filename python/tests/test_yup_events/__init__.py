655b79735104a9aeeaed60212d45d90b66047fd1
kunitoki <kunitoki@gmail.com>
2025-07-11 00:13:13 +0200

    Disable temporarily yup_events python tests

--- python/tests/test_yup_events/__init__.py
+++ python/tests/test_yup_events/__init__.py
@@ -1,8 +1,9 @@
 import pytest
+import sys
 
 from .. import common
 
 import yup
 
-if not hasattr(yup, "MessageManager") or not hasattr(yup, "YUPApplication"):
+if sys.platform != "darwin" or not hasattr(yup, "MessageManager") or not hasattr(yup, "YUPApplication"):
     pytest.skip(allow_module_level=True)
