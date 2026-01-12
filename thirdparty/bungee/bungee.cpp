0820f2ca2d3b8c229b74f3d1496b69ab268663f4
kunitoki <kunitoki@gmail.com>
2026-01-12 09:40:53 +0100

    Added bungee

---
+++ thirdparty/bungee/bungee.cpp
@@ -0,0 +1,37 @@
+/*
+  ==============================================================================
+
+   This file is part of the YUP library.
+   Copyright (c) 2026 - kunitoki@gmail.com
+
+   YUP is an open source library subject to open-source licensing.
+
+   The code included in this file is provided under the terms of the ISC license
+   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
+   to use, copy, modify, and/or distribute this software for any purpose with or
+   without fee is hereby granted provided that the above copyright notice and
+   this permission notice appear in all copies.
+
+   YUP IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+  ==============================================================================
+*/
+
+#include "bungee.h"
+
+#include "src/Assert.cpp"
+#include "src/Fourier.cpp"
+#include "src/Grain.cpp"
+#include "src/Grains.cpp"
+#include "src/Input.cpp"
+#include "src/Instrumentation.cpp"
+#include "src/Output.cpp"
+#include "src/Partials.cpp"
+#include "src/Stretch.cpp"
+#include "src/Stretcher.cpp"
+#include "src/Synthesis.cpp"
+#include "src/Timing.cpp"
+#include "src/version.cpp"
+#include "src/Window.cpp"
---
+++ thirdparty/bungee/bungee.h
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+   This file is part of the YUP library.
+   Copyright (c) 2026 - kunitoki@gmail.com
+
+   YUP is an open source library subject to open-source licensing.
+
+   The code included in this file is provided under the terms of the ISC license
+   http://www.isc.org/downloads/software-support-policy/isc-license. Permission
+   to use, copy, modify, and/or distribute this software for any purpose with or
+   without fee is hereby granted provided that the above copyright notice and
+   this permission notice appear in all copies.
+
+   YUP IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
+   EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
+   DISCLAIMED.
+
+  ==============================================================================
+*/
+
+/*
+  ==============================================================================
+
+  BEGIN_YUP_MODULE_DECLARATION
+
+    ID:               bungee
+    vendor:           dr_libs
+    version:          2.4.8
+    name:             Bungee is a modern, open-source C++ library for high-quality audio time-stretching and pitch-shifting in real-time or offline
+    description:      Bungee is a modern, open-source C++ library for high-quality audio time-stretching and pitch-shifting in real-time or offline.
+    website:          https://github.com/mackron/dr_libs
+    license:          MPL2.0
+
+    dependencies:     pffft eigen
+    searchpaths:      upstream
+    defines:          BUNGEE_USE_PFFFT=1
+
+  END_YUP_MODULE_DECLARATION
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <bungee/Bungee.h>
+#include <bungee/Stream.h>
