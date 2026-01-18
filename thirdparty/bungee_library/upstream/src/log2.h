806f52cd954db4a56151438908ed3867f890ed15
kunitoki <kunitoki@gmail.com>
2026-01-18 19:03:14 +0100

    Fix issue on missing methods on older android NDKs

--- thirdparty/bungee_library/upstream/src/log2.h
+++ thirdparty/bungee_library/upstream/src/log2.h
@@ -5,10 +5,51 @@
 
 #include "Assert.h"
 
-#include <bit>
+#include <limits>
 #include <type_traits>
 
 namespace Bungee {
+
+template <class T>
+constexpr int bit_width(T x) noexcept
+{
+    static_assert(std::is_integral_v<T>, "T must be an integral type");
+    using U = std::make_unsigned_t<T>;
+
+    U u = static_cast<U>(x);
+
+    if (u == 0)
+        return 0;
+
+    int width = 0;
+    while (u != 0)
+    {
+        u >>= 1;
+        ++width;
+    }
+
+    return width;
+}
+
+template <class T>
+constexpr int countr_zero(T x) noexcept
+{
+    static_assert(std::is_integral_v<T>, "T must be an integral type");
+    using U = std::make_unsigned_t<T>;
+    U u = static_cast<U>(x);
+
+    if (u == 0)
+        return std::numeric_limits<U>::digits; // number of bits in type
+
+    int count = 0;
+    while ((u & 1) == 0)
+    {
+        u >>= 1;
+        ++count;
+    }
+
+    return count;
+}
 
 template <bool floor = false>
 static inline int log2(unsigned x)
@@ -18,9 +59,9 @@ static inline int log2(unsigned x)
 
 	int y;
 	if constexpr (floor)
-		y = std::bit_width(x) - 1;
+		y = bit_width(x) - 1;
 	else
-		y = std::countr_zero(x);
+		y = countr_zero(x);
 
 	BUNGEE_ASSERT1(floor ? (1 << y <= x && x < 2 << y) : (x == 1 << y));
 	return y;
