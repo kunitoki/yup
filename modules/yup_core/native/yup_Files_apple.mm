a8505508dbc7d9f0bf855c69610fcd03494df1b4
kunitoki <kunitoki@gmail.com>
2026-05-22 20:42:37 +0200

    Make File constructor explicit

--- modules/yup_core/native/yup_Files_apple.mm
+++ modules/yup_core/native/yup_Files_apple.mm
@@ -306,6 +306,7 @@ File File::getSpecialLocation(const SpecialLocationType type)
             case userDocumentsDirectory:
                 resultPath = MacFileHelpers::getIOSSystemLocation(NSDocumentDirectory);
                 break;
+
             case userDesktopDirectory:
                 resultPath = MacFileHelpers::getIOSSystemLocation(NSDesktopDirectory);
                 break;
@@ -313,7 +314,7 @@ File File::getSpecialLocation(const SpecialLocationType type)
             case tempDirectory:
             {
                 File tmp(MacFileHelpers::getIOSSystemLocation(NSCachesDirectory));
-                tmp = tmp.getChildFile(yup_getExecutableFile().getFileNameWithoutExtension());
+                tmp = tmp.getChildFile(File(yup_getExecutableFile().getFileNameWithoutExtension()));
                 tmp.createDirectory();
                 return tmp.getFullPathName();
             }
@@ -322,6 +323,7 @@ File File::getSpecialLocation(const SpecialLocationType type)
             case userDocumentsDirectory:
                 resultPath = "~/Documents";
                 break;
+
             case userDesktopDirectory:
                 resultPath = "~/Desktop";
                 break;
@@ -336,21 +338,27 @@ File File::getSpecialLocation(const SpecialLocationType type)
             case userMusicDirectory:
                 resultPath = "~/Music";
                 break;
+
             case userMoviesDirectory:
                 resultPath = "~/Movies";
                 break;
+
             case userPicturesDirectory:
                 resultPath = "~/Pictures";
                 break;
+
             case userApplicationDataDirectory:
                 resultPath = "~/Library";
                 break;
+
             case commonApplicationDataDirectory:
                 resultPath = "/Library";
                 break;
+
             case commonDocumentsDirectory:
                 resultPath = "/Users/Shared";
                 break;
+
             case globalApplicationsDirectory:
                 resultPath = "/Applications";
                 break;
