385fde02b1069849d628db44574348a2c85799fa
kunitoki <kunitoki@gmail.com>
2026-07-06 22:38:00 +0200

    Remove stuff

--- thirdparty/rive_renderer/source/shaders/d3d/color_ramp.hlsl
+++
@@ -1,6 +0,0 @@
-#include "hlsl.minified.glsl"
-#include "constants.minified.glsl"
-
-#include "flush_uniforms.minified.glsl"
-#include "common.minified.glsl"
-#include "color_ramp.minified.glsl"
--- thirdparty/rive_renderer/source/shaders/d3d/tessellate.hlsl
+++
@@ -1,7 +0,0 @@
-#include "hlsl.minified.glsl"
-#include "constants.minified.glsl"
-
-#include "flush_uniforms.minified.glsl"
-#include "common.minified.glsl"
-#include "bezier_utils.minified.glsl"
-#include "tessellate.minified.glsl"
